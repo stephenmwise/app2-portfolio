1e20a7e5caf35415696967c59e69e2fbab2d50ba
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-09 12:54:25 -0400

    fixes column method name and added additional info row.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-col1, col2 = st.comumns(2)
+col1, col2 = st.columns(2)
+row_additional_info = st.columns(1)
 
 with col1:
     st.image("images/photo.png")
@@ -10,7 +11,12 @@ with col2:
     st.title("Stephen Wise")
     content = """
     Hi, I'm Stephen! I'm a Python programmer, and here's a portfolio showcasing my featured projects.
-    I spent 10+ years as a digital marketer, designer, and web developer, and in 2023 I decided to do a career change
-    into focusing on Python development, learning machine learning, data science, and web app development.
+    I spent 10+ years as a digital marketer, designer, and web developerIn. I have worked at Apple Design-winning app studios,
+    NASCAR's video game studio, Ecommerce sites, Devops agencies, and AI startups.
+    In 2023 I decided to do a career change into focusing on Python development, learning machine learning, data science, and web app development.
     """
     st.info(content)
+
+
+with row_additional_info:
+    textwrap("Below, you'll find my recent featured apps I've created as a showcase of my projects.")
